296b4d23cbae87f5007f007c00bdd4d67ece0a32
Cathy Camps <cathycamps@live.fr>
2023-02-16 12:53:18 +0100

    add components banner and card

--- src/components/Banner/IndexB.jsx
+++ src/components/Banner/IndexB.jsx
@@ -1,8 +1,15 @@
+import React from 'react'
+import banner from './assets/banner.png'
+import styles from '../Banner/banner.module.css'
+
 function Banner() {
     return (
-        <div>
-            <h1>I'm a banner</h1>
-        </div>
+        <div className={styles.banner}>
+            <img src={image} alt="Bannière" />
+            <div className={styles.banner-title}>
+            <h1>Chez vous, partout et ailleurs</h1>
+            </div>
+        </div>    
     )
 }
 
--- src/components/Card/IndexC.jsx
+++ src/components/Card/IndexC.jsx
@@ -1,1 +1,18 @@
 //import PropTypes from 'prop-types'
+import Properties from '../Properties'
+import styles from '../Card/card.module.css'
+
+function Card() {
+    return (
+        <section className={styles.section-card}>
+            <article 
+            key={element.id}
+            className={styles.card}>
+                <div>
+                    <h2 className={card-title}>{element.title}</h2>
+                </div>
+            </article>
+        </section>
+    )
+}
+export default Card
