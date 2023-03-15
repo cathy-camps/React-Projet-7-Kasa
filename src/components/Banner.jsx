079804a4c7e333bf1ba28fe99fd213258be548ed
Cathy Camps <cathycamps@live.fr>
2023-03-15 11:20:21 +0100

    modifs responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,18 +3,12 @@ import styles from '../Styles/banner.module.css'
 
 function Banner() {
   return (
-    <div className={styles.banner_parent}>
-      <div className={styles.banner}>
-        <img
-          className={styles.banner_img}
-          src={banner}
-          alt="Bannière paysage"
-        />
-        <div>
-          <h1 className={styles.banner_title}>
-            Chez vous, <br /> partout et ailleurs
-          </h1>
-        </div>
+    <div className={styles.banner}>
+      <img className={styles.banner_img} src={banner} alt="Bannière paysage" />
+      <div>
+        <h1 className={styles.banner_title}>
+          Chez vous, <br /> partout et ailleurs
+        </h1>
       </div>
     </div>
   )
